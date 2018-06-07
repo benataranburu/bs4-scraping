cd9381520dadff08bb79d146fa8d7d8f6c5233e6
Beñat Aranburu <baranburu@gmail.com>
2018-06-07 17:46:19 +0200

    Comments

--- navigation/navigate.py
+++ navigation/navigate.py
@@ -1,9 +1,11 @@
 import bs4 as bs
 import urllib.request
 
+#load soup
 src = urllib.request.urlopen('https://www.bbc.co.uk/news').read()
+#parse
 content = bs.BeautifulSoup(src,'lxml')
 
-# find a certain class
+# find occurrences of a certain class
 for h3 in content.find_all('h3','gs-c-promo-heading__title gel-paragon-bold nw-o-link-split__text'):
     print(h3.text)
