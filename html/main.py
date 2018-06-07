7bb08cdecd120697c358d1b0fab12b10fe49027a
Beñat Aranburu <baranburu@gmail.com>
2018-06-07 17:47:18 +0200

    Comments added

--- html/main.py
+++ html/main.py
@@ -1,9 +1,11 @@
 import bs4 as bs
 import urllib.request
 
+#load soup
 src = urllib.request.urlopen('https://bbc.co.uk/news').read()
-
+#parse
 content = bs.BeautifulSoup(src,'lxml')
 
+#read all pharagraphs
 for paragraph in content.find_all('p'):
     print(str(paragraph.text))
