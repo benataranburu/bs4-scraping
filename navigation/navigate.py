cd4684be294a9677417a83879e0d5b7aa1c2cd6d
Beñat Aranburu <baranburu@gmail.com>
2018-06-11 22:06:27 +0200

    Exception handling
    Exception handling for urlopen() method

--- navigation/navigate.py
+++ navigation/navigate.py
@@ -2,10 +2,16 @@ import bs4 as bs
 import urllib.request
 
 #load soup
-src = urllib.request.urlopen('https://www.bbc.co.uk/news').read()
-#parse
-content = bs.BeautifulSoup(src,'lxml')
+try:
+    src = urllib.request.urlopen('https://www.bbc.co.uk/news').read()
+except HTTPError as e:
+    print(e)
+except URLError:
+    print("Server down or incorrect domain")
+else:
+    #parse
+    content = bs.BeautifulSoup(src,'lxml')
 
-# find occurrences of a certain class
-for h3 in content.find_all('h3','gs-c-promo-heading__title gel-paragon-bold nw-o-link-split__text'):
-    print(h3.text)
+    # find occurrences of a certain class
+    for h3 in content.find_all('h3','gs-c-promo-heading__title gel-paragon-bold nw-o-link-split__text'):
+        print(h3.text)
